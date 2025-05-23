594f5842a9c974b1afc1d4705a81f74c272395c8
axun <140686872+2697182245@users.noreply.github.com>
2025-05-23 20:42:39 +0800

    初始文件 登录实现

---
+++ src/Beans/User.java
@@ -0,0 +1,27 @@
+package com.example.Beans;
+
+public class User {
+    String username;
+    String password;
+    String usertype;
+    public User(String username, String password, String usertype) {}
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getUsertype() {
+        return usertype;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+}
---
+++ src/Beans/student.java
@@ -0,0 +1,59 @@
+package com.example.Beans;
+
+public class student {
+    int id;
+    String name;
+    String sex;
+    int age;
+    double weight;
+    double height;
+    public student(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
